efc58d6c3821d6d682cc8dedb6d8fe644d32a5d3
k4yp <k4ypos@proton.me>
2023-08-29 17:37:15 +1000

    number parser

--- main.hs
+++ main.hs
@@ -1,2 +1,21 @@
+import System.IO
+
+data Node = Node [Char] [Char]
+
+parse :: [Char] -> Node
+parse input = do
+    number input
+
+number :: [Char] -> Node
+number value = do 
+   Node "number" value
+
+stringify :: Node -> [Char]
+stringify (Node str num) = "type: " ++ str ++ ", value: " ++ num
+
+main :: IO ()
 main = do
-    print "hello world"
+    contents <- readFile "main.en"
+    putStrLn contents
+
+    putStrLn (stringify (parse contents))
